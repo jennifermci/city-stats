8192daecd31b6ae21df487fa4abc53ee8070a026
Jennifer McIlvaine <jennymci22@gmail.com>
2020-02-24 15:47:56 -0800

    changed the city AQI

--- pollution_app/urls.py
+++ pollution_app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/pollution', views.pollution),
+    path('', views.pollution),
 ]
--- pollution_app/views.py
+++ pollution_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
 from .models import *
 from django.contrib import messages
-import bcrypt
+import requests
+
 
 def pollution(request):
-    return render(request,'pollution.html')
+    url = 'https://api.waqi.info/feed/{}/?token=7d948b48a98a28662fa192ada26908dab5923434'
+    city = 'Seattle'
+
+    r = requests.get(url.format(city)).json()
+
+    city_AQI = {
+        'city' : city,
+        'aqi' : r['data']['aqi'],
+        'impact': r['status'],
+
+    }
+    context = {
+        'city_AQI' : city_AQI
+    }
+
+    return render(request,'pollution.html', context)
